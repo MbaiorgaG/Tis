393ba220c28253bc927f26b9786cd04a2e862a0d
Gabriel Simon Mbaiorga <msgtoolz@gmail.com>
2022-11-22 12:19:07 +0100

    create tis-trade project

---
+++ tis-trade/src/main/java/com/sdl/tistrade/TisTradeApplication.java
@@ -0,0 +1,13 @@
+package com.sdl.tistrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TisTradeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TisTradeApplication.class, args);
+  }
+
+}
---
+++ tis-trade/src/test/java/com/sdl/tistrade/TisTradeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdl.tistrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TisTradeApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
